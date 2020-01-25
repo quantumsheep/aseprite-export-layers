0b85dff394d97536fe874b7ba49b5dfdba0a01a0
Nathanael Demacon <nathanael.dmc@outlook.fr>
2020-01-25 17:59:02 +0100

    Add trim option and clean code

--- export-each-layers.lua
+++ export-each-layers.lua
@@ -1,30 +1,50 @@
 local spr = app.activeSprite
 
-local path, title = spr.filename:match("^(.+[/\\])(.-).([^.]*)$")
+local spr_path, spr_title = spr.filename:match("^(.+[/\\])(.-).([^.]*)$")
 
-local msg = {"Do you want to export/overwrite the following files?"}
+local dlg = Dialog()
+dlg:file{
+    id = "output",
+    label = "Output:",
+    filename = spr_path .. spr_title .. '.png',
+    filetypes = {"png", "jpg", "jpeg"}
+}
+dlg:check{id = "trim", label = "Trim:"}
+dlg:button{id = "ok", text = "OK"}
+dlg:button{id = "cancel", text = "Cancel"}
+dlg:show()
 
-for i, layer in ipairs(spr.layers) do
-    local fn = path .. title .. '-' .. layer.name
-    table.insert(msg, '-' .. fn .. '.png')
+local data = dlg.data
+
+function fullpath(path, name, layer, extension)
+    return path .. name .. '-' .. layer.name .. '.' .. extension
 end
 
-if app.alert {
-    title = "Export Sprite Sheets",
-    text = msg,
-    buttons = {"&Yes", "&No"}
-} ~= 1 then return end
+if data.ok then
+    local path, name, extension = data.output:match("^(.+[/\\])(.-).([^.]*)$")
 
-for i, layer in ipairs(spr.layers) do
-    local fn = path .. '/' .. title .. '-' .. layer.name
+    local msg = {"Do you want to export/overwrite the following files?"}
 
-    app.command.ExportSpriteSheet {
-        ui = false,
-        type = SpriteSheetType.HORIZONTAL,
-        textureFilename = fn .. '.png',
-        layer = layer.name,
-        listLayers = false,
-        listTags = false,
-        listSlices = false
-    }
+    for _, layer in ipairs(spr.layers) do
+        table.insert(msg, '- ' .. fullpath(path, name, layer, extension))
+    end
+
+    if app.alert {
+        title = "Export Sprite Sheets",
+        text = msg,
+        buttons = {"&Yes", "&No"}
+    } ~= 1 then return end
+
+    for _, layer in ipairs(spr.layers) do
+        app.command.ExportSpriteSheet {
+            ui = false,
+            type = SpriteSheetType.HORIZONTAL,
+            textureFilename = fullpath(path, name, layer, extension),
+            layer = layer.name,
+            listLayers = false,
+            listTags = false,
+            listSlices = false,
+            trim = data.trim
+        }
+    end
 end
